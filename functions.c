c8f7b9feeefd617fd7ff2c3670f5f206b99545eb
99002763 <72455156+99002763@users.noreply.github.com>
2020-10-08 21:45:24 +0530

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,12 @@
+#include "header.h"
+
+int checkPrimeNumber(int n) {
+    int j, flag = 1;
+    for (j = 2; j <= n / 2; ++j) {
+        if (n % j == 0) {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
