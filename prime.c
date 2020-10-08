ebf5377dee3e27babe0f923eb401478633e92f65
99002763 <72455156+99002763@users.noreply.github.com>
2020-10-08 21:42:59 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -17,13 +17,4 @@ int main() {
 }
 
 // user-defined function to check prime number
-int checkPrimeNumber(int n) {
-    int j, flag = 1;
-    for (j = 2; j <= n / 2; ++j) {
-        if (n % j == 0) {
-            flag = 0;
-            break;
-        }
-    }
-    return flag;
-}
+
